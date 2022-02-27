7848b640fd7f6dedc14fd2660c6e65ceee24f852
v24kuon <63438234+v24kuon@users.noreply.github.com>
2022-02-27 11:04:33 +0900

    useState勉強内容1

---
+++ src/components/UseState1.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const UseState1 = () => {
+  const style: { [key: string]: string } = { marginBottom: '5rem' };
+  const [count, setCount] = useState<number>(0);
+  const [count2, setCount2] = useState<number>(0);
+  return (
+    <div>
+      <div {...{ style }}>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            setCount(count + 1);
+          }}
+        >
+          Count
+        </button>
+        <p>{count}</p>
+        <p>2回書いているが1つづしかカウントされない</p>
+      </div>
+      <div {...{ style }}>
+        <button
+          onClick={() => {
+            setCount2((pre) => pre + 1);
+            setCount2((pre) => pre + 1);
+          }}
+        >
+          Count
+        </button>
+        <p>{count2}</p>
+        <p>変数に入れることで2足される</p>
+      </div>
+    </div>
+  );
+};
+
+export default UseState1;
